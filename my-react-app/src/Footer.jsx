9a530ea72e6b8c508fa11a7639180a0a43468d5b
Nikita-Vaid <vaidnikita06@gmail.com>
2024-04-19 09:33:18 +0530

    1st commit

---
+++ my-react-app/src/Footer.jsx
@@ -0,0 +1,13 @@
+
+
+function Footer (){
+
+    return(
+        <footer>
+            {/* react ke ander js use karne ke lie {} curly bracses ka use karna hoga  */}
+        <p>&copy; { new Date().getFullYear()} Your Website name</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,22 @@
+// function based Component
+
+function Header() {
+    return (
+        // we can write pure html here 
+        <header>
+            <h1>
+                My Website
+            </h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
