ba05d97e5349aa4cb5a3d7cf9e0df750a740866d
Nikita-Vaid <vaidnikita06@gmail.com>
2024-04-26 15:35:49 +0530

    added

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -19,6 +19,8 @@ A parent compenent can send data to a child component.
 import Card from "./Card.jsx"
 import Student from "./Student.jsx";
 import UserGreeting from "./UserGreeting.jsx";
+import List from "./List.jsx";
+
 
 function App() {
   return (
@@ -26,11 +28,12 @@ function App() {
       <Card />
 
       <Student name="Spongbob" age={30} isStudent={true} />
-      <Student name="Patrick" age={42} isStudent={false} />
-      <Student name="Sandy" age={25} isStudent={true} />
+      {/* <Student name="Patrick" age={42} isStudent={false} /> */}
+      {/* <Student name="Sandy" age={25} isStudent={true} /> */}
       <Student name="Saboo" />
 
       <UserGreeting isLoggedIn={false} username="GirlTech"/>
+      <List/>
     </>
 
 
--- my-react-app/src/List.jsx
+++ my-react-app/src/List.jsx
@@ -1,7 +1,17 @@
 // List rendering 
 
-function List(){
-     const fruits = [];
+function List() {
+     const fruits = [{ id: 1, name: "apple", calories: 95 },
+     { id: 2, name: "banana", calories: 45 },
+     { id: 3, name: "pineapple", calories: 105 },
+     { id: 4, name: "orange", calories: 159 }];
+
+     // fruits.sort((a,b) => a.name.localeCompare(b.name));  // ALPHABETICAL
+     fruits.sort((a,b) => b.name.localeCompare(a.name));  // REVERSE ALPHABETICAL
+     fruits.sort((a,b) => a.calories - b.calories);  // NUMERIC 
+
+     const listItems = fruits.map(fruit => <li key={fruit.id}>{fruit.name}: &nbsp; <b>{fruit.calories}</b></li>);
+     return (<ul>{listItems}</ul>);
 }
 
-export   default List;
+export default List;
